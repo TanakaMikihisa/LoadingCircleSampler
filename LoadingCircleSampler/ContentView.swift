08f310e91eff642c699b9a19341d7a16fadd105f
Tanaka Mikihisa <minemiki0901@gmail.com>
2025-07-17 14:22:09 +0900

    Initial Commit

---
+++ LoadingCircleSampler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoadingCircleSampler
+//
+//  Created by tanakamiki on 2025/07/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoadingCircleSampler/LoadingCircleSamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingCircleSamplerApp.swift
+//  LoadingCircleSampler
+//
+//  Created by tanakamiki on 2025/07/17.
+//
+
+import SwiftUI
+
+@main
+struct LoadingCircleSamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
